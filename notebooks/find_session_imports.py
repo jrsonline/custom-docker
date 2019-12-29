5cc571fafae29cc776cf6d63006936f030195d41
strictlyswift <jrsonline@me.com>
2019-12-29 13:34:23 -0500

    Added a helpful comment to the output

--- notebooks/find_session_imports.py
+++ notebooks/find_session_imports.py
@@ -5,5 +5,6 @@ for i in In:
         library = i.split()[1]
         imports[library] = pkg_resources.get_distribution(library).version
 
+print("# Add the below line/s to your requirements.txt file")
 for imp in imports:
     print("{}=={}".format(imp,imports[imp]))
