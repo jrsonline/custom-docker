581043734c02e01d491f7c743b77c5d716fa337d
strictlyswift <jrsonline@me.com>
2019-12-29 23:38:29 -0500

    Now recursively finds dependencies

--- notebooks/find_session_imports.py
+++ notebooks/find_session_imports.py
@@ -1,9 +1,26 @@
 import pkg_resources
+
+def _set_version_of(library, imports):
+    try:
+        imports[library] = pkg_resources.get_distribution(library).version
+    except pkg_resources.DistributionNotFound:
+        pass
+
+def _get_deps_for(library, imports):
+    try:
+        deps = pkg_resources.working_set.by_key[library]
+        for r in deps.requires():
+            _set_version_of(r.project_name, imports)
+            _get_deps_for(r.project_name, imports)
+    except KeyError:
+        pass
+    
 imports={}
 for i in In:
-    if i.find("import") is 0 and i.find("pkg_resources") is -1 :
+    if i.find("import") is 0:
         library = i.split()[1]
-        imports[library] = pkg_resources.get_distribution(library).version
+        _set_version_of(library, imports)
+        _get_deps_for(library, imports)
 
 print("# Add the below line/s to your requirements.txt file")
 for imp in imports:
