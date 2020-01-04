395726527dffdc996980b8f3edd6c505e64850c9
strictly swift <jrsonline@me.com>
2020-01-04 13:03:56 -0500

    Update find_session_imports.py
    Made import check more sophisticated (able to cope with 'import library.something' ) and better logging.

--- notebooks/find_session_imports.py
+++ notebooks/find_session_imports.py
@@ -1,4 +1,8 @@
 import pkg_resources
+import re
+
+def _splitnl(str):
+    return str.split('\n')
 
 def _set_version_of(library, imports):
     try:
@@ -18,10 +22,12 @@ def _get_deps_for(library, imports):
 imports={}
 for i in In:
     if i.find("import") is 0:
-        library = i.split()[1]
+        library = re.split(r'\W+',i)[1]
+        print(f"...have found an import of '{library}' in jupyter notebook history (from line '{_splitnl(i)[0]}')")
         _set_version_of(library, imports)
         _get_deps_for(library, imports)
 
 print("# Add the below line/s to your requirements.txt file")
 for imp in imports:
     print("{}=={}".format(imp,imports[imp]))
+    
